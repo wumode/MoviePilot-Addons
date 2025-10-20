787c3bf60af48d85717f4d9ee89fbb260e639b21
wumode <wumo1999@gmail.com>
2025-10-20 12:02:10 +0800

    test addon update

--- plugins.v2/webdavstorage/__init__.py
+++ plugins.v2/webdavstorage/__init__.py
@@ -20,7 +20,7 @@ class WebdavStorage(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/wumode/MoviePilot-Addons/refs/heads/main/icons/disk.png"
     # 插件版本
-    plugin_version = "1.0.3"
+    plugin_version = "1.0.4"
     # 插件作者
     plugin_author = "wumode"
     # 作者主页
